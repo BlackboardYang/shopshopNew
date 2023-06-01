7951dc98ce36c97f67503394536f48062265cf0a
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2023-06-01 23:50:58 +0900

    OpenAPI3.0 Deployed
    CRUD ok!!!

--- shopshop-spring/shopshop/src/main/java/com/panacea/shopshop/ShopshopApplication.java
+++ shopshop-spring/shopshop/src/main/java/com/panacea/shopshop/ShopshopApplication.java
@@ -1,11 +1,7 @@
 package com.panacea.shopshop;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.panacea.shopshop.mapper.ProductMapper;
 
 @SpringBootApplication
 public class ShopshopApplication{
@@ -13,11 +9,5 @@ public class ShopshopApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ShopshopApplication.class, args);
 	}
-//	
-//	private final ProductMapper productMapper;
-//	
-//	public ShopshopApplication(ProductMapper productMapper) {
-//		this.productMapper = productMapper;
-//	}
 
 }
