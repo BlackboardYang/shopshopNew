b765a8873789f6433d9103f0bac266f0eb0ace6d
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2023-07-10 15:29:26 +0900

    springboot /api add
    server.servlet.context-path=/api

--- shopshop-vue/vite.config.js
+++ shopshop-vue/vite.config.js
@@ -14,6 +14,10 @@ const toggleDark = useToggle(isDark)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        host: true,
+        port: 80,
+    },
   plugins: [vue(),
   AutoImport({
                resolvers: [ElementPlusResolver()],
